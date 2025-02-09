0181aaf7ea921f06a474603930f7cedd450db9bf
Priya Acharjee <priyaacharjee2001@gmail.com>
2025-02-09 12:19:51 +0530

    updating

--- src/BinaryTrees3.java
+++ src/BinaryTrees3.java
@@ -62,6 +62,94 @@ public class BinaryTrees3 {
         Node lca = path1.get(i-1);
         return lca;
     }
+
+    public static Node lca2(Node root,int n1,int n2){
+
+        if(root==null|| root.data==n1 || root.data==n2){
+           return root;
+        }
+        Node leftlca=lca2(root.left,n1,n2);
+        Node rightlca=lca2(root.right,n1,n2);
+
+        //leftlca=val rightlca=null
+        if(rightlca==null){
+            return leftlca;
+        }
+        //rightlca=val leftlca=null
+        if(leftlca==null){
+            return rightlca;
+        }
+        return root;
+    }
+    public static int lcaDist(Node root,int n){
+        if(root==null){
+            return -1;
+        }
+        if(root.data==n){
+            return 0;
+        }
+        int leftDist=lcaDist(root.left,n);
+        int rightDist=lcaDist(root.right,n);
+
+        if(leftDist==-1 && rightDist==-1){
+            return -1;
+        }else if(leftDist==-1){
+            return rightDist+1;
+        }else{
+            return leftDist+1;
+        }
+    }
+    public static int minDist(Node root,int n1,int n2){
+        Node lca=lca2(root,n1,n2);
+        int dist1=lcaDist(lca,n1);
+        int dist2=lcaDist(lca,n2);
+
+        return dist1+dist2;
+    }
+    public static int KAncestor(Node root,int n,int k){
+        if(root==null){
+            return -1;
+        }
+        if(root.data==n){
+            return 0;
+        }
+        int leftDist=KAncestor(root.left,n,k);
+        int rightDist=KAncestor(root.right,n,k);
+
+        if(leftDist==-1 && rightDist==-1){
+            return -1;
+        }
+
+        int max=Math.max(leftDist,rightDist);
+        if(max+1==k){
+            System.out.println(root.data);
+        }
+        return max+1;
+    }
+
+    public static int sumTree(Node root){
+        if(root==null){
+            return 0;
+        }
+        int leftChild=sumTree(root.left);
+        int rightChild=sumTree(root.right);
+
+        int data=root.data;
+        int newLeft=root.left==null?0:root.left.data;
+        int newRight=root.right==null?0:root.right.data;
+
+        root.data=newLeft+leftChild+newRight+rightChild;
+        return data;
+    }
+
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.println(root.data +" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
     public static void main(String[] args) {
        Node root=new Node(1);
        root.left=new Node(2);
@@ -73,8 +161,15 @@ public class BinaryTrees3 {
 //       int k=3;
 //       KLevel(root,1,k);
 
-         int n1=4,n2=7;
-        System.out.println(lca(root,n1,n2).data);
+        // int n1=4,n2=5;
+        //System.out.println(lca(root,n1,n2).data);
+       // System.out.println(lca2(root,n1,n2).data);
+        //System.out.println(minDist(root,n1,n2));
+//        int n=4,k=1;
+//        KAncestor(root,n,k);
+
+        sumTree(root);
+        preorder(root);
 
     }
 }
