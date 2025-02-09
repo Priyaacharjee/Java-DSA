1fb228a427aca5b4bd9950df0a78f4d9459d7075
Priya Acharjee <priyaacharjee2001@gmail.com>
2025-02-09 09:42:24 +0530

    adding

--- src/BinaryTrees2.java
+++ src/BinaryTrees2.java
@@ -55,28 +55,28 @@ public class BinaryTrees2 {
         }
 
         //Approach 2
-        static class Info{
-            int diam;
-            int ht;
-
-            public Info(int diam,int ht){
-                this.diam=diam;
-                this.ht=ht;
-            }
-        }
-        public static Info diameter2(Node root){  //O(n)
-            if(root==null){
-                return new Info(0,0);
-            }
-            Info leftInfo=diameter2(root.left);
-            Info rightInfo=diameter2(root.right);
-
-            int diam=Math.max(Math.max(leftInfo.diam,rightInfo.diam),leftInfo.ht+ rightInfo.ht+1);
-            int ht=Math.max(leftInfo.ht,rightInfo.ht)+1;
-
-            return new Info(diam,ht);
-        }
-    }
+//        static class Info{
+//            int diam;
+//            int ht;
+//
+//            public Info(int diam,int ht){
+//                this.diam=diam;
+//                this.ht=ht;
+//            }
+//        }
+//        public static Info diameter2(Node root){  //O(n)
+//            if(root==null){
+//                return new Info(0,0);
+//            }
+//            Info leftInfo=diameter2(root.left);
+//            Info rightInfo=diameter2(root.right);
+//
+//            int diam=Math.max(Math.max(leftInfo.diam,rightInfo.diam),leftInfo.ht+ rightInfo.ht+1);
+//            int ht=Math.max(leftInfo.ht,rightInfo.ht)+1;
+//
+//            return new Info(diam,ht);
+//        }
+   }
 
     public static boolean isIdentical(Node node,Node subRoot){
         if(node==null && subRoot==null){
@@ -107,6 +107,53 @@ public class BinaryTrees2 {
         return leftAns || rightAns;
     }
 
+    static class Info{
+      Node node;
+      int hd;
+
+      public Info(Node node,int hd){
+          this.node=node;
+          this.hd=hd;
+      }
+    }
+
+    public static void topView(Node root){
+        //Level order
+        Queue<Info> q=new LinkedList<>();
+        HashMap<Integer,Node>map=new HashMap<>();
+
+        int min=0,max=0;
+        q.add(new Info(root,0));
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Info curr=q.remove();
+            if(curr==null){
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else {
+                if (!map.containsKey(curr.hd)) { //first time my hd is occuring
+                    map.put(curr.hd, curr.node);
+                }
+                if (curr.node.left != null) {
+                    q.add(new Info(curr.node.left, curr.hd - 1));
+                    min = Math.min(min, curr.hd - 1);
+                }
+                if (curr.node.right != null) {
+                    q.add(new Info(curr.node.right, curr.hd + 1));
+                    max = Math.max(max, curr.hd + 1);
+                }
+            }
+        }
+        for(int i=min;i<=max;i++){
+            System.out.println(map.get(i).data+" ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) { //o(n)
 //        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 //        BinaryTree tree=new BinaryTree();
@@ -124,11 +171,12 @@ public class BinaryTrees2 {
         root.right.left=new Node(6);
         root.right.right=new Node(7);
 
-        Node subroot=new Node(2);
-        subroot.left=new Node(4);
-        subroot.right=new Node(5);
-
-        System.out.println(isSubtree(root,subroot));
+//        Node subroot=new Node(2);
+//        subroot.left=new Node(4);
+//        subroot.right=new Node(5);
+//
+//        System.out.println(isSubtree(root,subroot));
+        topView(root);
     }
 
 }
