643b5ab27d43f91402abcce5de29deead0b54801
Priya Acharjee <priyaacharjee2001@gmail.com>
2025-07-23 00:05:38 +0530

    Adding Graphs part 3

--- src/TopologicalSort.java
+++ src/TopologicalSort.java
@@ -25,6 +25,7 @@ public class TopologicalSort {
         graph[5].add(new Edge(5,0));
         graph[5].add(new Edge(5,2));
 
+
     }
 
     //O(V+E) //Using DFS Technique
@@ -54,10 +55,48 @@ public class TopologicalSort {
         s.push(curr);
     }
 
+    public static void topSortBFS(ArrayList<Edge> graph[]){
+        int indeg[]=new int[graph.length];
+        calcIndeg(graph,indeg);
+        Queue<Integer> q=new LinkedList<>();
+
+        for (int i = 0; i < indeg.length; i++) {
+            if(indeg[i] == 0){
+                q.add(i);
+            }
+        }
+
+        //bfs
+        while(!q.isEmpty()){
+            int curr=q.remove();
+            System.out.print(curr+" "); //topological sort print
+
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e=graph[curr].get(i);
+                indeg[e.dest]--;
+                if(indeg[e.dest]==0){
+                    q.add(e.dest);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void calcIndeg(ArrayList<Edge> graph[],int indeg[]){
+        for(int i=0;i<graph.length;i++){
+            int v=i;
+            for (int j = 0; j < graph[v].size(); j++) {
+                Edge e=graph[v].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int V=6;
         ArrayList<Edge> graph[]=new ArrayList[V];
         createGraph(graph);
-        topSort(graph);
+        //topSort(graph);
+        topSortBFS(graph);
     }
 }
