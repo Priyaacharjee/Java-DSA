ea152a120ccc72922f2bc273576a8f0bb0877d3e
Priya Acharjee <priyaacharjee2001@gmail.com>
2025-08-01 02:42:22 +0530

    Adding More DP files

---
+++ src/DP/WildcardMatching.java
@@ -0,0 +1,15 @@
+public class WildcardMatching {
+    public static boolean wildcardMatching(String s,String p){
+        int n=s.length();
+        int m=p.length();
+        int dp[][]=new int[n+1][m+1];
+        dp[0][0]= 1;
+        for(int i=0;i<n+1;i++){
+            
+        }
+    }
+    public static void main(String[] args) {
+        String s= "aa";
+        String p="*";
+    }
+}
